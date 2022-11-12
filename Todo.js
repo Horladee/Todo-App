cab076a6f8c134c7e2affe691346d50087a1789a
Mustapha Oladayo <107470357+Horladee@users.noreply.github.com>
2022-11-13 00:16:49 +0100

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,25 @@
+document.getElementById("push").onclick = () => {
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }else{
+        document.querySelector("#tasks").innerHTML += `
+        <div class="task">
+        <span id="taskname">
+        ${document.querySelector('#newtask input').value}
+        </span>
+
+        <button class="delete">
+        <i class="fa-solid fa-trash-can"></i>
+        </button>
+
+        </div>
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function() {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
